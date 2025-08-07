c6de21c68838a3af1118b9d4bbe93904cddc7005
Dominik Peric <peric.domini7@gmail.com>
2025-08-07 17:29:39 +0200

    Model Account, napravio RegisterForm, register(view) i register html

---
+++ kazaliste_project/kazaliste/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+class RegisterForm(forms.Form):
+    email = forms.EmailField(label='Email')
+    email_confirm = forms.EmailField(label='Potvrda emaila')
+    password = forms.CharField(label='Lozinka', widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Potvrda lozinke', widget=forms.PasswordInput)
+    first_name = forms.CharField(label='Ime')
+    last_name = forms.CharField(label='Prezime')
+    phone = forms.CharField(label='Telefon')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get("email")
+        email_confirm = cleaned_data.get("email_confirm")
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+
+        if email and email_confirm and email != email_confirm:
+            raise forms.ValidationError("E-mail adrese se ne podudaraju.")
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Lozinke se ne podudaraju.")
+
+        return cleaned_data
--- kazaliste_project/kazaliste/views.py
+++ kazaliste_project/kazaliste/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from .forms import RegisterForm
+from .models import Account
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
     return render(request, 'kazaliste/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            if User.objects.filter(email=form.cleaned_data['email']).exists():
+                form.add_error('email', "Email adresa je već iskorištena.")
+                return render(request, 'kazaliste/register.html', {'form': form})
+
+            user = User.objects.create_user(
+                username=form.cleaned_data['email'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password'],
+                first_name=form.cleaned_data['first_name'],
+                last_name=form.cleaned_data['last_name']
+            )
+
+            Account.objects.create(
+                user=user,
+                phone=form.cleaned_data['phone']
+            )
+
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegisterForm()
+    return render(request, 'kazaliste/register.html', {'form': form})
